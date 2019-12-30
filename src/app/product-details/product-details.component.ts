3df1118800714bedb1a031ec6bb1b82ed4b856a5
Vitalii <vetal.turbine@gmail.com>
2019-12-30 12:14:11 +0200

    Create the shopping cart service (https://angular.io/start/data#create-the-shopping-cart-service)

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 
 import {products} from "../products";
+import {CartService} from "../services/cart.service";
 
 @Component({
   selector: 'app-product-details',
@@ -13,6 +14,7 @@ export class ProductDetailsComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService
   ) {
   }
 
@@ -22,4 +24,9 @@ export class ProductDetailsComponent implements OnInit {
     });
   }
 
+  addToCart(product) {
+    window.alert('Product has been added!');
+
+    this.cartService.addToCart(product);
+  }
 }
